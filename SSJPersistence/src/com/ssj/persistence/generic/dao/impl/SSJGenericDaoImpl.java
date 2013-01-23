49e114e029b72200b0cc8d995c508894229bc19b
shoppingsaojao <shoppingsaojao@c232705d-fa3a-494c-9d21-1598dcdf697a>
2013-01-23 13:55:48 +0000

    Dao

--- SSJPersistence/src/com/ssj/persistence/generic/dao/impl/SSJGenericDaoImpl.java
+++ SSJPersistence/src/com/ssj/persistence/generic/dao/impl/SSJGenericDaoImpl.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ssj.persistence.generic.dao.SSJGenericDao;
